420499136f0f141cde3bc7f95a53d53178c94787
Christie <christie.ferderer@gmail.com>
2014-08-12 11:47:45 -0700

    part one of assignment

--- Class Roster/File.swift
+++ Class Roster/File.swift
@@ -25,25 +25,5 @@ class Person: NSObject {
         //return "/(firstName) /(lastName)"
         return self.firstName + " " + self.lastName
     }
-    
-    //func findImage (image : UIImage){
-       // self.image = image
-        //if there is a pic get true
-        //else false
-    //}
-    //func classMatesAlive () -> [Person]{
-        
-      //  var classMates = [Person] ()
-        //var firstNameArr = ["christie", "Nate", "Matthew", "Jeff", "John", "David", "Adrian", "Jake", "Tony", "Brad", "Shams", "Cameron", "Kori", "Parker", "Nathan", "Casey", "Brendan", "Brian", "Mark", "Rowan", "Lance", "Kevin", "Will", "Heather", "Tuan", "Zack",    "Sara", "Hongyao"]
-       // var lastNameArr = ["ferderer", "Birkholz", "Brightbill", "Chavez", "Clem", "Fry", "Petrica Gherle", "Hawken", "Huang", "Johnson", "Kazi", "Klein", "Kolodziejczak", "Lewis", "Ma", "MacPhee", "McAleer", "Mendez", "Morris", "North", "Park", "Pham", "Richman", "Theringer", "Vu", "Walkingstick", "Wong", "Zhang"]
-        
-        //var people = 0
-        //for i in 0...firstNameArr.count {
-          //  var people = Person(firstName: firstNameArr[i], lastName : lastNameArr[i])
-            
-        //}
-        //return [Person] ()
-        
-    //}
 
 }
--- Class Roster/ViewController.swift
+++ Class Roster/ViewController.swift
@@ -4,34 +4,22 @@
 //
 //  Created by Christie Ferderer on 8/7/14.
 //  Copyright (c) 2014 Christie Ferderer. All rights reserved.
-//
+//hello
 
 import UIKit
 
 class ViewController: UIViewController {
-   
     
     @IBOutlet var pressButton: UIButton!
                             
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-       // func classMatesAlive () -> [Person]{
+        }
+    
         var classMates = [Person] ()
         var firstNameArr = ["christie", "Nate", "Matthew", "Jeff", "John", "David", "Adrian", "Jake", "Tony", "Brad", "Shams", "Cameron", "Kori", "Parker", "Nathan", "Casey", "Brendan", "Brian", "Mark", "Rowan", "Lance", "Kevin", "Will", "Heather", "Tuan", "Zack",    "Sara", "Hongyao"]
         var lastNameArr = ["ferderer", "Birkholz", "Brightbill", "Chavez", "Clem", "Fry", "Petrica Gherle", "Hawken", "Huang", "Johnson", "Kazi", "Klein", "Kolodziejczak", "Lewis", "Ma", "MacPhee", "McAleer", "Mendez", "Morris", "North", "Park", "Pham", "Richman", "Theringer", "Vu", "Walkingstick", "Wong", "Zhang"]
-        //initializeRoserter (namefirst:
-        classMates = self.initializerRoster(studentFirst, nameLast: studentLast)
-        
-            for i in 0...firstNameArr.count {
-                println(classMates[i].fullName())
-        }
-        
-                var people = Person(firstName: firstNameArr[i], lastName : lastNameArr[i])
-                
-            }
-            return classMates
-    }
+    
     @IBAction func buttonPressed (sender: UIButton){
         
             }
